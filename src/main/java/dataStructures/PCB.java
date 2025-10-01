fac2951bc4a2da585dcdf60cc0617369cc26d882
Sanda2 <santiagoaristimuno2005@gmail.com>
2025-10-01 14:35:40 -0400

    pcb basic

---
+++ src/main/java/dataStructures/PCB.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dataStructures;
+
+   
+
+/**
+ *
+ * @author santi
+ */
+public class PCB {
+    //Identificacion del proceso
+    private int id;
+    private String name;
+    // Estado del procesador
+    private StateProcess state;
+    // Informacion de control
+    //TODO lo demas
+    
+}
