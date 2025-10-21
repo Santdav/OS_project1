acab4ed497916fa090217f042bef021f9036c191
Miguel Ignazio Greco Aponte <mgreco@correo.unimet.edu.ve>
2025-10-21 03:57:45 -0400

    Parte de la simulacion de hilo
    erm

---
+++ src/main/java/dataStructures/Enums/ProcessThread.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dataStructures.Enums;
+import dataStructures.ProcessQueue;
+import java.util.concurrent.Semaphore;
+
+/**
+ *
+ * @author Squidward
+ */
+public class ProcessThread implements Runnable{
+
+    public Process PCB; // PCB asociado a un "hilo" de ejecucion.
+    public int cycleDuration; // Duracion de ciclo
+    private Semaphore usingCpu;
+    private Semaphore readyQueueLock;
+    private Semaphore blockedQueueLock;
+    private ProcessQueue readyQueue;
+    private ProcessQueue blockedQueue;
+    private int cyclesSinceLastIO;
+
+    public ProcessThread(Process PCB, int cycleDuration, Semaphore usingCpu, Semaphore readyQueueLock, Semaphore blockedQueueLock, ProcessQueue readyQueue, ProcessQueue blockedQueue, int cyclesSinceLastIO) {
+        this.PCB = PCB;
+        this.cycleDuration = cycleDuration;
+        this.usingCpu = usingCpu;
+        this.readyQueueLock = readyQueueLock;
+        this.blockedQueueLock = blockedQueueLock;
+        this.readyQueue = readyQueue;
+        this.blockedQueue = blockedQueue;
+        this.cyclesSinceLastIO = cyclesSinceLastIO;
+    }
+    @Override
+    public void run() {
+        readyQueueLock.acquire();
+        readyQueue.enqueue(this.PCB);
+        readyQueueLock.release();
+    }
+    
+}
